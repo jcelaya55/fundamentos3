38be1d9acec9f0c6556c48662554020f7f6675c7
jcelaya55 <98981507+jcelaya55@users.noreply.github.com>
2022-11-28 20:00:03 -0700

    Add files via upload
    proyecto en equipo

---
+++ Bloque.java
@@ -0,0 +1,17 @@
+package Block_Chain;
+
+import java.util.Date;
+
+public class Bloque {
+
+    //Atributos
+    public String value;
+    public String Timestap;
+    public String Datos;
+    public String Hash;
+
+    public String Backhash;
+    
+  
+   
+}
